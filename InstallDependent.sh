35523556439885b03eefca6e85817370ec014146
Qiang <liqiangucas@outlook.com>
2020-06-14 12:31:37 +0200

    modified path

--- InstallDependent.sh
+++ InstallDependent.sh
@@ -1,25 +1,68 @@
 #!usr/bin/env bash
 
-# Copyright(c) 202 QiangLi
+# Copyright(c) 2020 QiangLi
 # All Rights Reserved.
 # qiang.li@uv.es
 # Distributed under the (new) BSD License.
 
-sudo pip3 install MotionClouds
-sudo pip3 install NeuroTools
-sudo pip3 install statsmodels==0.10.0rc2 --pre
-sudo pip3 install pyrtools
-sudo pip3 install PyWavelets
-sudo pip3 install colour-science
-sudo pip3 install colorama
-sudo pip3 install tensorflow
 
 # Install pytorch
 # The help link video: https://www.youtube.com/watch?v=15dzeLMFPC4.
 # Pytorch only works under the virtual environment.
 
-#   option1: source ~/.bashrc
-#   option3:  
+#   option1: source ~/.bashrc : (base) this conda environment.  
+#            conda deactivate : quit conda environment. 
+#   option2: mkdir virutal environment.
+#            cd virutal environment.
+#            python3 -m venv pytorch-BioMulti-L-NL  
+#            -------------------------------------------------
+#            source pytorch-BioMulti-L-NL/bin/activate
+#            deactivate 
+#            
+sudo pip3 install torch==1.3.0+cpu torchvision==0.4.1+cpu  -f https://download.pytorch.org/whl/torch_stable.html
+
+# Install option: (1) under conda environment- conda install  (2) venv-virutal environment-pip3 install
+sudo pip3 install numpy
+sudo pip3 install NeuroTools
+sudo pip3 install --user git+https://github.com/NeuralEnsemble/MotionClouds.git
+# Option: download then add path in local.
+#sudo pip3 install MotionClouds
+#conda install MotionClouds
+sudo pip3 install statsmodels==0.10.0rc2 --pre
+sudo pip3 install pyrtools
+# Install from source
+# git clone https://github.com/LabForComputationalVision/pyrtools
+# cd pyrtools
+# python setup.py -e 
+# cd ..
+sudo pip3 install PyWavelets
+#conda install pywavelets
+
+sudo pip3 install colour-science
+#sudo pip3 install colorama
+sudo pip3 install tensorflow
+#conda install tensorflow
+#conda install scipy
+#conda install scikit-image
+#conda install -c conda-forge opencv
+
+sudo pip3 install scipy
+#sudo python -m pip3 install scipy
+#sudo apt-get install python3-scipy
+sudo pip3 install opencv-python
+
+
+# numpy
+# scipy
+# NeuroTools
+# matplotlib
+# IPython
+# watermark
+# version_information
+# vispy
+# pyglet
+# imageio
+# pyopengl
 
 
 echo '===============================' \n
---
+++ main.sh
@@ -0,0 +1,11 @@
+#!usr/bin/env bash
+
+# Copyright(c) 202 QiangLi
+# All Rights Reserved.
+# qiang.li@uv.es
+# Distributed under the (new) BSD License.
+
+
+python BioMulti_L_NL_Model_FirstVersion.py
+
+echo 'Runing Finished'
