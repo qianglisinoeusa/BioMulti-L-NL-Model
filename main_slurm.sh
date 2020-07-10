0663df6a453f3bbfd062764006c7cbbff40994ac
Qiang <liqiangucas@outlook.com>
2020-07-10 18:15:52 +0200

    execute slurm

---
+++ main_slurm.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+SBATCH --ntasks=10
+SBATCH --job-name=RF_100
+SBATCH --output=rf_%j.out
+SBATCH --exclude=nodo17
+
+# Copyright(c) 202 QiangLi
+# All Rights Reserved.
+# qiang.li@uv.es
+# Distributed under the (new) BSD License.
+
+ 
+module load Anaconda3
+source activate your_virtual_environment
+ 
+python -u SimpleVisualModel.py
