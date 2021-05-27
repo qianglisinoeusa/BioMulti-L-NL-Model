77e5e2357185d773d89b80fcd3b7a7aeae19ef04
A.Lee <liqiangucas@outlook.com>
2021-05-27 14:44:23 +0200

    Update and rename JSignPdf to JSignPdf.java

---
+++ Java/JSignPdf.java
@@ -0,0 +1,4 @@
+
+// JSignPdf is a Java application which adds digital signatures to PDF documents.
+// The help doc can be link http://jsignpdf.sourceforge.net/. This GUI application can be avaiable both Windows and Linux OS. 
+java -jar JSignPdf.jar
